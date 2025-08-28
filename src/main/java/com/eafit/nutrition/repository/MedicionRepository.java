ba21caa8bd44bf74db680eda3cf4b028c2036857
unknown <jclondonoo@eafit.edu.co>
2025-08-27 22:10:24 -0500

    Uploading all the homework

---
+++ src/main/java/com/eafit/nutrition/repository/MedicionRepository.java
@@ -0,0 +1,25 @@
+package com.eafit.nutrition.repository;
+
+import com.eafit.nutrition.model.Medicion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import java.util.Optional;
+
+@Repository
+
+public interface MedicionRepository extends JpaRepository<Medicion, Long> {
+
+    List<Medicion> findByPacienteIdOrderByFechaDesc(Long pacienteId);
+
+    Optional<Medicion> findFirstByPacienteIdOrderByFechaDesc(Long pacienteId);
+
+    // Método para encontrar la última medición de un paciente
+    @Query(value = "SELECT * FROM medicion WHERE paciente_id = :pacienteId ORDER BY fecha DESC, id DESC LIMIT 1", nativeQuery = true)
+    Optional<Medicion> findLastMedicionByPacienteId(@Param("pacienteId") Long pacienteId);
+
+}
